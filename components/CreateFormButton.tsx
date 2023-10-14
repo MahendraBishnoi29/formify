d738fabec249e76dbb4fd02472b230caf041d556
Mahendra <mahendrabrowny@gmail.com>
2023-10-14 16:42:49 +0530

    create new form dialog done

---
+++ components/CreateFormButton.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { FC } from "react";
+import { BsFileEarmarkPlus } from "react-icons/bs";
+import { ImSpinner2 } from "react-icons/im";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { toast } from "./ui/use-toast";
+
+interface CreateFormButtonProps {}
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
+const formSchema = z.object({
+  name: z.string().min(4),
+  description: z.string().optional(),
+});
+
+const CreateFormButton: FC<CreateFormButtonProps> = ({}) => {
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: FormSchemaType) => {
+    try {
+    } catch (error) {
+      toast({
+        title: "Error Creating Form",
+        description: "Please try again later",
+        variant: "destructive",
+      });
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create New Form</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create form</DialogTitle>
+          <DialogDescription>
+            Create a new form to start collecting responses
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea rows={5} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            onClick={() => {
+              form.handleSubmit(onSubmit);
+            }}
+            disabled={form.formState.isSubmitting}
+            className="w-full mt-4"
+          >
+            {form.formState.isSubmitting ? (
+              <ImSpinner2 className="animate-spin" />
+            ) : (
+              <span className="">Create</span>
+            )}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateFormButton;
