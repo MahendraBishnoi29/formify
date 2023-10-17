acf891ef33eb1093c12a0f621400e76617ae081e
Mahendra <mahendrabrowny@gmail.com>
2023-10-17 19:55:54 +0530

    type file for react-dnd

---
+++ lib/types.ts
@@ -0,0 +1,32 @@
+import { TextFieldFormElement } from "@/components/fields/TextField";
+
+export type ElementsType = "TextField";
+
+export type FormElementInstance = {
+  id: string;
+  type: ElementsType;
+  extraAttributes?: Record<string, any>;
+};
+
+export type FormElement = {
+  type: ElementsType;
+
+  constructs: (id: string) => FormElementInstance;
+
+  designerBtnElement: {
+    icon: React.ElementType;
+    label: string;
+  };
+
+  designerComponent: React.FC;
+  formComponent: React.FC;
+  propertiesComponent: React.FC;
+};
+
+type FormElementsType = {
+  [key in ElementsType]: FormElement;
+};
+
+export const FormElements: FormElementsType = {
+  TextField: TextFieldFormElement,
+};
