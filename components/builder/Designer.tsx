45a4fdbe3f4d8da0622c9d565bc602ba787e687d
Mahendra <mahendrabrowny@gmail.com>
2023-10-23 15:27:37 +0530

    show elements on designer

--- components/builder/Designer.tsx
+++ components/builder/Designer.tsx
@@ -1,17 +1,40 @@
 "use client";
 
-import { useDroppable } from "@dnd-kit/core";
+import useDesigner from "@/hooks/useDesigner";
+import { ElementsType, FormElements } from "@/lib/types";
+import { cn, idGenerator } from "@/lib/utils";
+import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
 import { FC } from "react";
+import DesignerElementWrapper from "./DesignerElementWrapper";
 import DesignerSidebar from "./DesignerSidebar";
-import { cn } from "@/lib/utils";
 
 type DesignerProps = {};
 
 const Designer: FC<DesignerProps> = ({}) => {
+  const { elements, addElement } = useDesigner();
+
   const droppable = useDroppable({
     id: "designer-drop-area",
     data: {
       isDesignerDropArea: true,
+    },
+  });
+
+  useDndMonitor({
+    onDragEnd: (e: DragEndEvent) => {
+      const { active, over } = e;
+      if (!active || !over) return;
+
+      const isDesignerBtnElement = active.data?.current?.designerBtnElement;
+
+      if (isDesignerBtnElement) {
+        const type = active.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].constructs(
+          idGenerator()
+        );
+
+        addElement(0, newElement);
+      }
     },
   });
 
@@ -25,15 +48,25 @@ const Designer: FC<DesignerProps> = ({}) => {
             droppable.isOver && "ring-2 ring-primary/20"
           )}
         >
-          {!droppable.isOver ? (
+          {!droppable.isOver && elements?.length === 0 && (
             <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
               Drop here
             </p>
-          ) : (
+          )}
+
+          {droppable.isOver && (
             <div className="p-4 w-full">
               <div className="h-[120px] rounded-md bg-primary/20"></div>
             </div>
           )}
+
+          {elements?.length > 0 ? (
+            <div className="flex flex-col w-full gap-2 p-4 text-background">
+              {elements.map((elem) => (
+                <DesignerElementWrapper key={elem.id} element={elem} />
+              ))}
+            </div>
+          ) : null}
         </div>
       </div>
       <DesignerSidebar />
